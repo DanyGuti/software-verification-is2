067df52aef5f5599709da523b798a1c9d585f087
unknown <hugomr123@outlook.com>
2024-10-26 13:00:24 +0200

    implementacion de factory en cliente

--- src/main/java/gui/ApplicationLauncher.java
+++ src/main/java/gui/ApplicationLauncher.java
@@ -9,61 +9,15 @@ import javax.xml.ws.Service;
 
 import configuration.ConfigXML;
 import dataAccess.DataAccess;
+import factoryFacade.FactoryFacade;
 import businessLogic.BLFacade;
 import businessLogic.BLFacadeImplementation;
 
 public class ApplicationLauncher {
 
 	public static void main(String[] args) {
-
-		ConfigXML c = ConfigXML.getInstance();
-
-		System.out.println(c.getLocale());
-
-		Locale.setDefault(new Locale(c.getLocale()));
-
-		System.out.println("Locale: " + Locale.getDefault());
-
-		try {
-
-			BLFacade appFacadeInterface;
-			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-
-			if (c.isBusinessLogicLocal()) {
-
-				DataAccess da = new DataAccess();
-				appFacadeInterface = new BLFacadeImplementation(da);
-
-			}
-
-			else { // If remote
-
-				String serviceName = "http://" + c.getBusinessLogicNode() + ":" + c.getBusinessLogicPort() + "/ws/"
-						+ c.getBusinessLogicName() + "?wsdl";
-
-				URL url = new URL(serviceName);
-
-				// 1st argument refers to wsdl document above
-				// 2nd argument is service name, refer to wsdl document above
-				QName qname = new QName("http://businessLogic/", "BLFacadeImplementationService");
-
-				Service service = Service.create(url, qname);
-
-				appFacadeInterface = service.getPort(BLFacade.class);
-			}
-
-			MainGUI.setBussinessLogic(appFacadeInterface);
-			MainGUI a = new MainGUI();
-			a.setVisible(true);
-
-		} catch (Exception e) {
-			// a.jLabelSelectOption.setText("Error: "+e.toString());
-			// a.jLabelSelectOption.setForeground(Color.RED);
-
-			System.out.println("Error in ApplicationLauncher: " + e.toString());
-		}
-		// a.pack();
-
+		String isLocal = "local"; 
+		BLFacade blFacade = new FactoryFacade().createBLFacade(isLocal); 
+		System.out.println(blFacade);
 	}
-
 }
